43c4c3804ccdb9fb84e1e23879554da7f1bdd13a
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-06 09:51:50 -0300

    section 1 para cellular feita

---
+++ src/components/section1/rightSide.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const RightSide = () => {
+    return (
+        <div className="flex justify-center">
+            <Image alt="GROUP 50" src={`/assets/imgs/Group 50.png`} width={361} height={329}/>
+        </div>
+    );
+}
--- src/components/section1/section1Main.tsx
+++ src/components/section1/section1Main.tsx
@@ -1,9 +1,11 @@
 import { LeftSide } from "./leftSide";
+import { RightSide } from "./rightSide";
 
 export const Section1Main = () => {
     return (
-        <main className="container mx-auto p-4 lg:py-[24px] lg:px-[80px]">
+        <main className="container mx-auto py-[48px] px-4 lg:py-[24px] lg:px-[80px] flex flex-col gap-[48px]">
             <LeftSide />
+            <RightSide />
         </main>
     );
 }
