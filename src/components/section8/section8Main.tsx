3f1e43d85ad798626d8f786759ee43dd2898a9e6
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-08 11:03:45 -0300

    section 8 feita

--- src/components/section8/section8Main.tsx
+++ src/components/section8/section8Main.tsx
@@ -4,11 +4,13 @@ import { Infos } from "./infos";
 
 export const Section8Main = () => {
     return (
-        <main className="p-4">
-            <div className="flex flex-col gap-[32px] justify-center items-center">
-                <Image alt="img 1 section 8" src={`/assets/imgs/Cards Round.png`} width={361} height={167} />
+        <main className="p-4 lg:px-0 lg:py-[80px]">
+            <div className="flex flex-col gap-[32px] justify-center items-center lg:flex-row lg:justify-between">
+                <Image alt="img 1 section 8" src={`/assets/imgs/Cards Round.png`} width={361} height={167} className="lg:hidden"/>
+                <Image alt="img 1 section 8 pc" src={`/assets/imgs/Cards Roundpc.png`} width={467} height={636} className="hidden lg:flex"/>
                 <Infos />
-                <Image alt="img 2 section 8" src={`/assets/imgs/Cards Round2.png`} width={352} height={195}/>
+                <Image alt="img 2 section 8" src={`/assets/imgs/Cards Round2.png`} width={352} height={195} className="lg:hidden"/>
+                <Image alt="img 2 section 8 pc" src={`/assets/imgs/Cards Round2pc.png`} width={480} height={598} className="hidden lg:flex"/>
             </div>
         </main>
     );
